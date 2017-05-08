bc6d9cab0aac6154e4ccd4e502a8c63f8356ce04
PikminGuts92 <PikminGuts92@gmail.com>
2017-05-08 16:53:27 -0400

    Updated main app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import './board-state.ts';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-    title = 'Connect Fish'
+export class AppComponent implements OnInit {
+    public title: string = 'Connect Fish';
+    private game: BoardState;
+
+    ngOnInit() {
+        this.game = new BoardState(6, 7);
+    }
 }
