5ec8d39496e610c0d726316c6f8c6ebd029a6c31
PikminGuts92 <PikminGuts92@gmail.com>
2017-05-08 16:24:40 -0400

    Reformatted line tabs and deleted component spec file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'Put content in here...';
+    title = 'Connect Fish'
 }
